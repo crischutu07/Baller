a2e7eee61586a7df1fc900b77d41260fd441fd5f
Nguyễn Hồng Sơn <81855366+crischutu07@users.noreply.github.com>
2022-11-21 23:13:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo "[*] Upgrading Packages"
-apt update -y 1> /dev/null
-apt upgrade -y 1> /dev/null
+pkg update -y 1> /dev/null
+pkg upgrade -y 1> /dev/null
 echo "[*] Installing wget, curl, git, ruby"
 apt install wget curl git ruby -y 1> /dev/null
 echo "[*] Forking repository"
