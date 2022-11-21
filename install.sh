960055ea12497e2bf697e2830cc1980dc67e845f
Nguyễn Hồng Sơn <81855366+crischutu07@users.noreply.github.com>
2022-11-21 23:09:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apt upgrade -y 1> /dev/null
 echo "[*] Installing wget, curl, git, ruby"
 apt install wget curl git ruby -y
 echo "[*] Forking repository"
-fork https://github.com/crischutu07/Baller ./Baller/
+git clone https://github.com/crischutu07/Baller ./Baller/
 rm ./Baller/install.sh
 echo "[*] Done! Run ./start.sh to use."
 exit 0
