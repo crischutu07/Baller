fcc1c6519f1a121a1bc28b80d026acc71dc5f010
Nguyễn Hồng Sơn <81855366+crischutu07@users.noreply.github.com>
2022-11-23 05:46:31 +0700

    Fixing stuff
    Idk

--- start.sh
+++ start.sh
@@ -1,20 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
+source ./src/cmd.sh
 # This type of file must on Baller/ directories
-source ./src/cmd.sh 2>> err.log
-if [[ $(command -v unzip) == "" ]]; then
-  apt install unzip -y 1> /dev/null
-fi
-if [[ $(command -v git) == "" ]]; then
-  apt install git -y 1> /dev/null
-fi
-if [[ $(command -v wget) == "" ]]; then
-  apt install wget -y 1> /dev/null
-fi
-if [[ $(command -v lolcat) == "" ]]; then
-  echo -e "Warning: ${Red}You are not installed ${Yellow}'lolcat'${Red}, the script will automatically install."
-  echo -e "${Reset}Notice: ${Yellow}To avoiding this, run ${Green}./start.sh${Yellow} with ${Green}--no-ascii${Yellow} argument"
-  gem install lolcat 1> /dev/null
-fi
+dependencies=( unzip git wget lolcat jq nvim )
+# removed and replace this one idk
+for i in "${dependencies[@]}"; do
+  [[ $i == 'lolcat' ]] && package_manager='gem install' || package_manager='apt install'
+  [[ $(command -v "$i") == "" ]] && $package_manager "$i" 1> /dev/null
+done
+unset -v package_manager
+
 if [[ $1 != "--no-ascii" ]]; then
 echo """'########:::::'###::::'##:::::::'##:::::::'########:'########::
  ##.... ##:::'## ##::: ##::::::: ##::::::: ##.....:: ##.... ##:
@@ -28,24 +22,24 @@ echo """'########:::::'###::::'##:::::::'##:::::::'########:'########::
 """ | lolcat 2> /dev/null
 fi
 printf "Use ${Yellow}'help'${Reset} for a list of commands.\n"
+# random guy script
 until [[ $cmd == "#!" ]]; do (
   printf "${Reset}> "
 read cmd > /dev/null
 history -r .history
 case "$cmd" in
-  "shell"|"Shell") cmdShell 2>> err.log;;
-  "exit"|"quit"|"Exit"|"Quit") exit 0 2>> err.log;;
-  "help"|"Help") cmdHelp 2>> err.log;;
-  "license"|"License") cmdLicense 2>> err.log;;
-  "reload"|"Reload") cmdReload 2>> err.log;;
-  "battery"|"Battery") cmdBattery 2>> err.log;;
+  [Ss]hell) cmdShell 2>> err.log;;
+  [Ee]xit|[Qq]uit) exit 0 2>> err.log;;
+  [Hh]elp) cmdHelp 2>> err.log;;
+  [Ll]icense) cmdLicense 2>> err.log;;
+  [Rr]eload) cmdReload 2>> err.log;;
+  [Bb]attery) cmdBattery 2>> err.log;;
   "") :;;
-  "version") cmdVersion 2>> err.log;;
-  "editor") cmdEditor 2>> err.log;;
-  "history") cmdHistory 2>> err.log;;
-  "update") cmdUpdatePackage 2>> err.log;;
-  "momjoke") cmdMomJoke 2>> err.log;;
-  "dadjoke") cmdDadJoke 2>> err.log;;
+  [Vv]ersion) cmdVersion 2>> err.log;;
+  [Ee]ditor) cmdEditor 2>> err.log;;
+  [Hh]istory) cmdHistory 2>> err.log;;
+  [Uu]pdate) cmdUpdatePackage 2>> err.log;;
+  [Jj]oke) cmdJoke 2>> err.log;;
   *) echo -e "${Red}${cmd}${Reset} Not Found";;
 esac
 history -s "$cmd"
