e400c38b729a502267e01942ba6faea53c1e7826
Nguyễn Hồng Sơn <81855366+crischutu07@users.noreply.github.com>
2022-11-23 05:48:03 +0700

    Sources edit
    Removes Inappropriate joke
    Merge dad and mom jokes into 1  function

--- src/cmd.sh
+++ src/cmd.sh
@@ -1,4 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
+# THIS IS CONFIG, FUNCTION FILES
+# PLEASE DDN'T TOUCH!
+# RUN THIS BY TYPING -> bash src/cmd.sh IF YOU'RE ON MAIN FOLDER
 source ./src/color.sh
 cmdHelp (){
 echo -e """LIST OF COMMANDS:
@@ -10,15 +13,14 @@ ${Green}- ${Yellow}version ${Reset}(Print version of this bash files)
 ${Green}- ${Yellow}battery ${Reset}(Print battery)
 ${Green}- ${Yellow}history ${Reset}(Read history files)
 ${Green}- ${Yellow}update ${Reset}(Update package)
-${Green}- ${Yellow}momjoke ${Reset}(Tell me mom joke)
-${Green}- ${Yellow}dadjoke ${Reset}(Tell me dad joke)"""
+${Green}- ${Yellow}joke ${Reset}(Tell me dad or mom joke)"""
 }
 cmdLicense (){
   echo -e """MIT License
 Copyright (c) 2022 ${Yellow}crischutu07${Reset}
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
+of this software and associated documentation files (the \"Software\"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
@@ -27,13 +29,14 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE."""
+SOFTWARE.
+"""
 }
 cmdBattery (){
   battery=$(termux-battery-status)
@@ -44,17 +47,17 @@ cmdBattery (){
 fi
 }
 cmdShell() {
-  if [[ ! -z "$(command -v zsh)" ]]; then
-    zsh 2>> err.log
-  else
-    bash 2>> err.log
-  fi
+    if [[ $(command -v zsh ) == "" ]]; then
+        bash
+    else
+        zsh
+    fi
 }
 cmdEditor (){
-  nvim $HOME/../
+  eval $EDITOR $HOME/../
 }
 cmdVersion (){
-  echo -e "Baller Shell ${Green}v1.0.0"
+  echo -e "Baller Shell ${Green}v1.0.0 (Termux ${Yellow}v${TERMUX_VERSION})${Reset}"
 }
 cmdHistory (){
   echo "zombie q" > .screenrc
@@ -66,9 +69,13 @@ cmdUpdatePackage (){
   pkg upg -y 1> /dev/null
   echo "Done!"
 }
-cmdMomJoke (){
-  curl https://api.yomomma.info/ | jq -r ".joke"
-}
-cmdDadJoke (){
-  shuf -n 1 dad.txt
+cmdJoke (){
+  echo "Select Joke:"
+  echo -e "[${Green}1${Reset}]${Yellow} Dad Joke${Reset}"
+  printf "[${Green}2${Reset}]${Yellow} Mom Joke${Reset}\n→ " && read joke
+  case $joke in
+    "1") curl https://api.yomomma.info/ | jq -r ".joke";;
+    "2") shuf -n 1 src/dad.txt;;
+    *) echo -e "${Red}Number is required${Reset}";;
+  esac
 }
